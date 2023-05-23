1e8799d66060fc5797273a6a1831a105851dfe18
Ananasovna <a.n.ushkova@yandex.ru>
2023-05-23 18:00:06 +0200

    init: initialize repo

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
+
   document.getElementById('root') as HTMLElement
 );
+
+let a = [1, 2, 3,4];
+
+
+
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <div></div>
   </React.StrictMode>
 );
 
@@ -17,3 +25,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
